1069c52f6c3519b22385c30fcff952db059e68b2
tatsuponbipanya <tatsuponbipanya@gmail.com>
2019-09-12 16:44:33 +0900

    プロジェクト ファイルを追加します。

---
+++ Fusen/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Fusen
+{
+    partial class FormFusen
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textFusenMemo = new System.Windows.Forms.TextBox();
+            this.colorDialogFusen = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // textFusenMemo
+            // 
+            this.textFusenMemo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textFusenMemo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textFusenMemo.Location = new System.Drawing.Point(0, 0);
+            this.textFusenMemo.Multiline = true;
+            this.textFusenMemo.Name = "textFusenMemo";
+            this.textFusenMemo.Size = new System.Drawing.Size(800, 450);
+            this.textFusenMemo.TabIndex = 0;
+            this.textFusenMemo.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextFusenMemo_KeyDown);
+            this.textFusenMemo.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.TextFusenMemo_MouseDoubleClick);
+            this.textFusenMemo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TextFusenMemo_MouseDown);
+            this.textFusenMemo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TextFusenMemo_MouseMove);
+            // 
+            // FormFusen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textFusenMemo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormFusen";
+            this.Opacity = 0.6D;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textFusenMemo;
+        private System.Windows.Forms.ColorDialog colorDialogFusen;
+    }
+}
+
---
+++ Fusen/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fusen
+{
+    public partial class FormFusen : Form
+    {
+        private int mouseX;     //マウスの横の位置
+        private int mouseY;     //マウスの縦の位置
+
+
+        public FormFusen()
+        {
+            InitializeComponent();
+        }
+
+        private void TextFusenMemo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();       //ESCボタンでアプリ終了
+            }
+        }
+
+        private void TextFusenMemo_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.mouseX = e.X;      //左クリックした場合、マウスの位置を記憶
+                this.mouseY = e.Y;
+            }
+        }
+
+        private void TextFusenMemo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - mouseX;      //マウスを移動させた場合、フォームの位置を更新
+                this.Top += e.Y - mouseY;
+            }            
+        }
+
+        private void TextFusenMemo_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            colorDialogFusen.ShowDialog();      //ダブルクリックした時、色の設定ダイアログを表示
+            textFusenMemo.BackColor = colorDialogFusen.Color;       //背景を選んだ色に変更
+        }
+    }
+}
